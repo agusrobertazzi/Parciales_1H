0e1329911694a4e0e69db83ebbc8cd9bb1c2aaa7
Agus Robertazzi <robertazziagustin1806@gmail.com>
2021-10-18 03:56:56 -0300

    Parcial_01

---
+++ parcial_01/src/defines.h
@@ -0,0 +1,20 @@
+/*
+ * defines.h
+ *
+ *  Created on: 16 oct. 2021
+ *      Author: agust
+ */
+
+#ifndef DEFINES_H_
+#define DEFINES_H_
+
+#define TRUE 0
+#define FALSE -1
+#define SALONES_LEN 100
+#define ARCADE_LEN 1000
+#define SALON 0
+#define SHOPPING 1
+#define MONO 0
+#define ESTEREO 1
+
+#endif /* DEFINES_H_ */
